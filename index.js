663ef0f7b4f15b80fdbadcd2483a10def2df817f
Adarsh Malviya <adarshmalviya1199@gmail.com>
2022-11-19 00:43:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let scoreHome =0;
+let scoreGuest = 0;
+let home = document.getElementById("score-home");
+let guest = document.getElementById("score-guest")
+
+function homePlus1(){
+    scoreHome += 1;
+    home.textContent = scoreHome;
+}
+function homePlus2(){
+    scoreHome += 2;
+    home.textContent = scoreHome;
+}
+function homePlus3(){
+    scoreHome += 3;
+    home.textContent = scoreHome;
+}
+
+function guestPlus1(){
+    scoreGuest += 1;
+    guest.textContent = scoreGuest;
+}
+function guestPlus2(){
+    scoreGuest += 2;
+    guest.textContent =scoreGuest;
+}
+function guestPlus3(){
+    scoreGuest += 3;
+    guest.textContent = scoreGuest;
+}
